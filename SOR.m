cd01675eac082cba45de46dd317cf24cde25c65f
Jinney Kil <jinneykil@gmail.com>
2015-03-03 22:23:53 -0800

    SOR Iteration Method

--- SOR.m
+++ SOR.m
@@ -1,14 +1,21 @@
-function x=SOR(a, b, x0, tol, w)
+function [x, k]=SOR(a, b, x0, tol, w)
+% This function uses the iterative method: Successive Over Relaxation (SOR)
+% INPUT
+% a: matrix A in Ax=b
+% b: matrix b in Ax=b
+% x0: initial x^(0)
+% tol: absolute tolerance
+% w: scalar w
+% OUTPUT
+% x: solution
+% k: number of iterations to meet tolerance (tol)
 
-n=length(b);
-sum1=zeros(n,1);
-counter = 0;
+n=length(b);    % number of unknowns
+sum1=zeros(n,1);    
 x=x0;
 xn=x0;
-k=0;
+k=0;    % initializes iteration counter
 
-% while tol<norm(xn-x)
-while counter<N+1
 for i=1:n
     for j=1:n
         if j==i
@@ -17,11 +24,24 @@ for i=1:n
             sum1(j)=-(w*a(i,j)/a(i,i))*x(j);
         end
     end
-    xn(i)=x(i);
+    xn=x;
     x(i)=sum(sum1)+(w*b(i)/a(i,i))+((1-w)*(x(i)));
-    counter=counter+1;
     sum1=0;
 end
 k=k+1;
-
+    
+while tol<norm(xn-x)
+    for i=1:n
+        for j=1:n
+            if j==i
+                sum1(j)=0;
+            else
+                sum1(j)=-(w*a(i,j)/a(i,i))*x(j);
+            end
+        end
+        xn=x;
+        x(i)=sum(sum1)+(w*b(i)/a(i,i))+((1-w)*(x(i)));
+        sum1=0;
+    end
+    k=k+1;
 end
